eac2202b03f278a776e4b89e81254e724170eafd
Andreas Norberg Bergström <andorojas@gmail.com>
2023-04-20 23:10:35 +0200

    Add basic styling to generated grid

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,13 @@ let grid = document.getElementById("grid");
 //Generate tilegrid
 function generateTileGrid(){
 
-    //Clears grid before creating new ones
+    //Clears grid before creating new tiles
     clearTileGrid();
 
     //Sets the grid size to fit
     grid.style.gridTemplateColumns = `repeat(${columnAmount.value}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${rowAmount.value}, 1fr)`;
-    
+
     //Creates a new .tile div for every column in every row
     for(let i = 0; i < rowAmount.value; i++){
         
@@ -20,7 +20,7 @@ function generateTileGrid(){
             let tile = document.createElement('div');
             tile.className="tile";
             tile.innerText=`${i+1}|${j+1}`
-
+            
             grid.appendChild(tile);
         }
     }
