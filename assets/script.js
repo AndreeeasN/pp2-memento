36932455e9062a53b8a8311537362b5e4deb7277
Andreas Norberg Bergström <andorojas@gmail.com>
2023-04-24 20:11:45 +0200

    Add reveal/hide timers for correct tiles and implement basic score

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,26 @@ let levelResetButton = document.getElementById("levelReset");
 let levelUpButton = document.getElementById("levelUp");
 let grid = document.getElementById("grid");
 let tilesArray = [];
+let selectedTiles = [];
 let correctTiles = [];
 let level = 1;
 let maxLevel = 64;
-//Difficulty is used for grid size (3 is 3x3, 4 is 4x4 etc.)
 let difficulty = 3;
-let mode = "select";
-
-//GRID GENERATION 
+let score = 0;
+let userCanInteract = false;
+let userCorrectSelections = 0;
+let userWrongSelections = 0;
 
 //Generate tilegrid
 function generateTileGrid(){
+    //Ensures user can't interact with tiles and resets selections
+    userCanInteract = false;
+    resetPlayerSelections();
 
     //Clears grid before creating new tiles
     clearTileGrid();
 
-    //Sets the grid size to fit all tiles
+    //Sets the grid size to fit all tiles (difficulty decides grid size, difficulty*difficulty)
     grid.style.gridTemplateColumns = `repeat(${difficulty}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${difficulty}, 1fr)`;
 
@@ -38,7 +42,7 @@ function generateTileGrid(){
     tilesArray = Array.from(document.getElementsByClassName("tile"));
 }
 
-//Generate an array of the tiles to be remembered
+//Generate an array of correct tile numbers to be remembered
 function generateCorrectTiles(){
     //The amount of correct tiles needed, we'll start with 2 tiles on level 1
     let neededTiles = level + 1;
@@ -51,30 +55,72 @@ function generateCorrectTiles(){
             correctTiles.push(num);
         }        
     }
+}
 
-    //DEBUG - COLORS CORRECT TILES
+//Shows all correct tiles
+function revealCorrectTiles(){
     let tiles = document.getElementsByClassName("tile");
     for(let index of correctTiles){
         tiles[index].style.backgroundColor="white";
     }
 }
 
+//Resets all tile colors and allows user interaction
+function hideCorrectTiles(){
+    let tiles = document.getElementsByClassName("tile");
+    for(let i of tiles){
+        i.style.backgroundColor="#666";
+    }
+    userCanInteract = true;
+}
+
+//All requirements for starting new level
+function newLevel(){
+    //Generates grid
+    generateTileGrid();
+    generateCorrectTiles();
+
+    //Waits before revealing all correct tiles
+    setTimeout(() => {
+        revealCorrectTiles();
+    }, 500);
+
+    //Waits before hiding tiles
+    setTimeout(() => {
+        hideCorrectTiles();
+    }, 2000);
+}
 
 function tileInteract(){
-
     //Reveals tile if it exists in correctTiles[] and user is allowed to select
-    if(mode === "select"){
+    if(userCanInteract){
         //Index of the selected tile
         let tileIndex = tilesArray.indexOf(this)
         let tiles = document.getElementsByClassName("tile");
 
+        //If selected tile is correct
         if (correctTiles.includes(tileIndex)){
-            
             tiles[tileIndex].style.backgroundColor="lightgreen";
+            score++;
+            userCorrectSelections++;
         }
-        else{
+        //If selected tile is incorrect
+        else
+        {
             tiles[tileIndex].style.backgroundColor="lightcoral";
+            userWrongSelections++;
         }
+    }
+
+    //Disables input and waits before moving to the next level
+    if(userCorrectSelections >= correctTiles.length){
+        userCanInteract = false;
+        resetPlayerSelections();
+
+        setTimeout(() => {
+            levelUp();
+            newLevel();
+        }, 1000);
     }
 }
 
@@ -84,28 +130,33 @@ function clearTileGrid(){
     correctTiles=[];
 }
 
-//Resets level
-function levelReset(){
-    if(level>1){
-        level=1;
-        calculateDifficulty();
-    }
-
-    generateTileGrid();
-    generateCorrectTiles();
-}
-
 //Increases level
 function levelUp(){
     if(level<maxLevel){
         level++;
         calculateDifficulty();
     }
-
-    generateTileGrid();
-    generateCorrectTiles();
 }
 
+//Resets level, used on restart
+function levelReset(){
+    if(level>1){
+        level=1;
+        calculateDifficulty();
+    }
+    resetPlayerSelections();
+    newLevel();
+}
+
+
+//Resets amount of selected tiles
+function resetPlayerSelections(){
+    selectedTiles = [];
+    userCorrectSelections = 0;
+    userWrongSelections = 0;
+}
+
+//Difficulty is used for grid size (3 is 3x3, 4 is 4x4 etc.)
 function calculateDifficulty(){
     //Increases difficulty every 3rd level
     if (level % 3 === 1){
@@ -117,5 +168,4 @@ function calculateDifficulty(){
 levelResetButton.addEventListener("click", levelReset);
 levelUpButton.addEventListener("click", levelUp);
 
-generateTileGrid();
-generateCorrectTiles();
+newLevel();
